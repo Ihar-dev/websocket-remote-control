9dcaade69b96ee544a8be20825b37f62656d9fcb
Ihar Kozik <ihar.kozik@gmail.com>
2022-06-23 17:45:48 +0300

    feat: add mouse_position command

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -21,20 +21,25 @@ wss.on('connection', async ws => {
     console.log(`Client has sent us: ${data}`);
     const textData = data.toString();
     const mouse = robot.getMousePos();
-    const command = textData.split(' ')[0];
-    const moveData = textData.split(' ')[1];
+    const textDataArr = textData.split(' ');
+    let command = textDataArr[0];
+    let options: string | null;
+    (textDataArr.length > 1) ? options = textDataArr[1] : options = null;
     switch (command) {
       case 'mouse_up':
-        robot.moveMouse(mouse.x, mouse.y - Number(moveData));
+        robot.moveMouse(mouse.x, mouse.y - Number(options));
       break;
       case 'mouse_down':
-        robot.moveMouse(mouse.x, mouse.y + Number(moveData));
+        robot.moveMouse(mouse.x, mouse.y + Number(options));
       break;
       case 'mouse_left':
-        robot.moveMouse(mouse.x - Number(moveData), mouse.y);
+        robot.moveMouse(mouse.x - Number(options), mouse.y);
       break;
       case 'mouse_right':
-        robot.moveMouse(mouse.x + Number(moveData), mouse.y);
+        robot.moveMouse(mouse.x + Number(options), mouse.y);
+      break;
+      case 'mouse_position':
+        command += ` ${mouse.x},${mouse.y}`;
       break;
       default:
     }
